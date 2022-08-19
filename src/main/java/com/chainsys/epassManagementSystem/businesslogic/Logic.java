c8ccd6d0132e31b14e3adde693230be21a794940
NaveenRomi <naveenromi27@gmail.com>
2022-08-19 12:15:00 +0530

    clearing bugs

--- src/main/java/com/chainsys/epassManagementSystem/businesslogic/Logic.java
+++ src/main/java/com/chainsys/epassManagementSystem/businesslogic/Logic.java
@@ -1,31 +1,12 @@
 package com.chainsys.epassmanagementsystem.businesslogic;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
 import java.time.LocalDate;
 
 public class Logic {
 	
 	private Logic(){
 	}
-	public static Date getInstanceDate1() {
-		Calendar vcalendar = Calendar.getInstance();
-		String dates = vcalendar.get(Calendar.DATE) + "/" + (vcalendar.get(Calendar.MONTH) + 1) + "/"
-				+ vcalendar.get(Calendar.YEAR);
-		SimpleDateFormat dateFormat = new SimpleDateFormat();
-		Date date = null;
-		try {
-			date = new java.sql.Date(dateFormat.parse(dates).getTime());
-
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		return date;
-	}
-
+	
 	public static LocalDate getInstanceDate() {
 		return java.time.LocalDate.now();
 	}
